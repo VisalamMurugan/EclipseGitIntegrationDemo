cff864cb60f6fda79a40b594fb0a1a3933711293
VisalamMurugan <visalam.murugan@gmail.com>
2020-06-23 15:29:18 +0530

    Release V1.0 changes are commited

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,9 +5,9 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to java ,base master!!!");
+		System.out.println("Welcome to java ,Release branch!!!");
 
-		System.out.println(Eclipse with Github Integration  Thank you!!!");
+		System.out.println(Eclipse with Github Integration - Child level validation Thank you!!!");
 
 	}
 }
