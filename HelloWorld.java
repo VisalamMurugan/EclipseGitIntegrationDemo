8f1d7de9259ba601dc36244a11f6181efebc7697
VisalamMurugan <visalam.murugan@gmail.com>
2020-06-23 16:42:34 +0530

    Release V1.0 - version committed

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to java ,Release branch!!!");
+		System.out.println("Welcome to java ,Release - branch!!!");
 
 		System.out.println(Eclipse with Github Integration - Child level validation Thank you!!!");
 
